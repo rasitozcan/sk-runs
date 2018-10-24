7754c076048b72720e7ac08285acb0c94aed92bb
rasitozcan <ozcanrasit@gmail.com>
2018-10-25 01:27:10 +0300

    fix

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -1,11 +1,11 @@
 import CMS from 'netlify-cms';
 
-import AboutPagePreview from './preview-templates/AboutPagePreview';
+import AllRunnersPagePreview from './preview-templates/AllRunnersPagePreview';
 import BlogPostPreview from './preview-templates/BlogPostPreview';
 import RunnerPreview from './preview-templates/RunnerPreview';
 import ProductPagePreview from './preview-templates/ProductPagePreview';
 
-CMS.registerPreviewTemplate('about', AboutPagePreview);
+CMS.registerPreviewTemplate('kosucular', AllRunnersPagePreview);
 CMS.registerPreviewTemplate('products', ProductPagePreview);
 CMS.registerPreviewTemplate('blog', BlogPostPreview);
 CMS.registerPreviewTemplate('runners', RunnerPreview);
--- src/cms/preview-templates/AllRunnersPagePreview.js
+++ src/cms/preview-templates/AllRunnersPagePreview.js
@@ -1,19 +1,19 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import { AboutPageTemplate } from '../../templates/about-page'
+import React from 'react';
+import PropTypes from 'prop-types';
+import { AllRunnersPageTemplate } from '../../templates/allrunners-page';
 
 const AboutPagePreview = ({ entry, widgetFor }) => (
-  <AboutPageTemplate
+  <AllRunnersPageTemplate
     title={entry.getIn(['data', 'title'])}
     content={widgetFor('body')}
   />
-)
+);
 
 AboutPagePreview.propTypes = {
   entry: PropTypes.shape({
     getIn: PropTypes.func,
   }),
   widgetFor: PropTypes.func,
-}
+};
 
-export default AboutPagePreview
+export default AboutPagePreview;
